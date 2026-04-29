cff1eebe1a2114fddcc06baa563562fcf9f670e9
iRainy22 <rainier.ring@gmail.com>
2026-04-29 11:17:46 -0700

    Add, two distance unit tests

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -14,9 +14,62 @@ class GuesserTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-// Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
-{
-  Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+
+// TEST Distance
+/*
+  Returns an whole number representing the distance between the guess,
+  provided as an argument, and the secret. The distance represents the number
+  of characters that would have to be changed at the same location to match
+  the other string. When the two strings are identical, the distance is 0,
+  but for each letter different, the distance increases by 1. When the
+  strings are different lengths, each of the extra characters in the longer
+  string also counts as an increase in 1 to the distance, up to the length of
+  the secret; in other words, if m_secret has a 10 characters and the guess
+  has 100, the distance is 10.
+*/
+TEST(GuesserTest, D_EmptySecret) {
+    Guesser object("");
+    int actual = Guesser.distance("AAA");
+    ASSERT_EQ(0, actual);
+
 }
+
+TEST(GuesserTest, D_OneOff) {
+    Guesser object("ABC");
+    int actual = Guesser.distance("ABD");
+    ASSERT_EQ(1, actual)
+}
+
+// TEST Match
+/*
+  Determines and returns whether the provided guess matches the secret
+  phrase. However, the function also returns false if the secret is locked,
+  which happens if either (or both): 
+    (A) there are no remaining guesses allowed
+    (B) the function detects brute force, as indicated by a guess that has a
+        distance greater than 2 from the secret
+  See the distance() functions for specifications regarding determining the 
+  distance between a guess and the secret.
+  
+  A Guesser object allows up to
+  three (3) consecutive guesses without a match. If three guesses are made
+  without any being a true match, the secret is locked. However, whenever
+  an unlocked secret is guessed with a true match, the guesses remaining
+  reset to three (3). If the secret is locked for any other reason, such
+  as a big distance in a guess, the count of remaining guesses should still
+  count down as usual to hide that the secret has been locked.
+*/
+
+
+
+
+// TEST Guesser
+/*
+  Constructor requires that the secret phrase is provided its value as
+  an argument. This secret will not change for the lifespan of an instance
+  of any Guesser object and must have a length of 32 characters or less,
+  otherwise, it will be truncated at that length.
+*/
+
+
+
